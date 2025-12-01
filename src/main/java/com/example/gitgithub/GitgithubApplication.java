f85e81368e5f413daeb072c8b7c5cf151cbcd9e3
derbe <derbely887@gmail.com>
2025-12-01 11:25:58 +0100

    feat: afficher User et Product au démarrage

--- src/main/java/com/example/gitgithub/GitgithubApplication.java
+++ src/main/java/com/example/gitgithub/GitgithubApplication.java
@@ -6,11 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitgithubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitgithubApplication.class, args);
-		file1 file = new file1();
-		System.out.println(file.getMessage());
-		
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GitgithubApplication.class, args);
+        
+        // BUG SIMULÉ
+        file1 file = new file1();
+        System.out.println("Message: " + file.getMessage());
+        System.out.println("Author: " + file.getAuthor());
+        
+        User user = new User(1L, "yassine", "yassine@example.com");
+        System.out.println("User créé: " + user);
+        
+        Product product = new Product(1L, "Laptop", 999.99, 10);
+        System.out.println("Product créé: " + product);
+    }
 }
