925a60f21799b4fb4c5109c2ce5faa16392e33cd
Eya Jarroudi <eyajarroudi52@gmail.com>
2025-12-01 17:36:09 +0100

    ADD aut

--- src/main/java/com/example/gitgithub/file1.java
+++ src/main/java/com/example/gitgithub/file1.java
@@ -2,8 +2,23 @@ package com.example.gitgithub;
 
 public class file1 {
     private String message = "Hello, GitHub!";
+    private String author = "Mohamed Yassine Derbel";
+
     public String getMessage() {
         return message;
     }
     
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
+    
+
