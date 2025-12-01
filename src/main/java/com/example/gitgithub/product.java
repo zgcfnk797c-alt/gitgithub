6fda860d48ec1dc8a71f5eba7bfb0ae9f847c5a5
Eya Jarroudi <eyajarroudi52@gmail.com>
2025-12-01 18:27:39 +0100

    add product

---
+++ src/main/java/com/example/gitgithub/product.java
@@ -0,0 +1,55 @@
+package com.example.gitgithub;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer stock;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Double price, Integer stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{id=" + id + ", name='" + name + "', price=" + price + ", stock=" + stock + "}";
+    }
+}
